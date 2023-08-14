7417833fcb3ec3b7f160eb3702851cfb535f0796
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-08-14 19:30:04 +0530

    UI and CRUD opeations done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/util/dialog_box.dart';
 import '../util/todo_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+  List toDoList = [
+    ['Make Tutorial', false],
+    ['Do exercise', false],
+  ];
+
+  //checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DialogBox(
+            controller: _controller,
+            onSave: saveNewTask,
+            onCancel: () => Navigator.of(context).pop(),
+          );
+        });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +55,29 @@ class HomePage extends StatelessWidget {
       backgroundColor: Color.fromARGB(255, 178, 104, 227),
       appBar: AppBar(
         centerTitle: true,
-        title: Text(
+        title: const Text(
           'TO DO',
           style: TextStyle(
             fontWeight: FontWeight.bold,
+            fontSize: 25,
           ),
         ),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ToDoTile(),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (context) => deleteTask(index),
+          );
+        },
       ),
     );
   }
